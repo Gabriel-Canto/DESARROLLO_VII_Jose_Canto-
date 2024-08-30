46c224177d6703812316c69b200a1f0800e28b64
Gabriel-Canto <143668412+Gabriel-Canto@users.noreply.github.com>
2024-08-29 20:14:03 -0500

    Inicialización del proyecto DESARROLLO_VII

---
+++ TALLERES/TALLER_2/perfil_usuario.php
@@ -0,0 +1,33 @@
+
+<?php
+// Definición de variables
+$nombre = "Jose Canto";
+$edad = 20;
+$correo = "jose.canto6@utp.ac.pa";
+$telefono = "6029-0564";
+
+// Definición de constante
+define("OCUPACION", "Estudiante");
+
+// Creación de mensaje usando diferentes métodos de concatenación e impresión
+$mensaje1 = "Hola, mi nombre es " . $nombre . "  tengo " . $edad . " años mi correo es" . $correo. "y mi telefono es" .$telefono ;
+$mensaje2 = "soy " . OCUPACION . ".";
+
+echo $mensaje1 . "<br>";
+print($mensaje2 . "<br>");
+
+printf("En resumen: %s, %d años, %s, %s<br>", $nombre, $edad, $correo, $telefono, OCUPACION);
+
+echo "<br>Información de debugging:<br>";
+var_dump($nombre);
+echo "<br>";
+var_dump($edad);
+echo "<br>";
+var_dump($correo);
+echo "<br>";
+var_dump($telefono);
+echo "<br>";
+var_dump(OCUPACION);
+echo "<br>";
+?>
+                    
