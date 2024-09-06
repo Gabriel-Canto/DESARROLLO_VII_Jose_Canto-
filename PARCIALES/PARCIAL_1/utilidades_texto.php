7346b303ed58a7d0457aca21d2985a9a9647c5b1
Gabriel-Canto <143668412+Gabriel-Canto@users.noreply.github.com>
2024-09-05 20:16:02 -0500

    creacion de funciones de contar_palabras e invertir_palabras

---
+++ PARCIALES/PARCIAL_1/utilidades_texto.php
@@ -0,0 +1,15 @@
+<?php
+$contar_palabras = explode(" ", $frase);
+echo "el numero de palabras es: ", count($contar_palabras), "<br><br>";
+
+
+$contar_vocales = strtolower($frase); 
+
+
+echo"el numero de vocales es: ", $contar_vocales, "<br><br>";
+
+$invertir_palabras = explode(" ", $frase);
+rsort($invertir_palabras);
+echo "la frase de forma invertida es: ". implode(" ", $invertir_palabras);
+
+?>
