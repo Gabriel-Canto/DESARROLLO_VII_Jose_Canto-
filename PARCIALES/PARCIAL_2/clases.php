a12a87c2e4d94f8d03893609c1895b70f5fb109b
Gabriel-Canto <143668412+Gabriel-Canto@users.noreply.github.com>
2024-10-10 20:10:39 -0500

    Subida Parcial 2

--- PARCIALES/PARCIAL_2/clases.php
+++ PARCIALES/PARCIAL_2/clases.php
@@ -1,14 +1,19 @@
 <?php
-class RecursoBiblioteca {
+interface obtenerDetallesEspecificos {
+    public function obtenerDetallesEspecificos(): string;
+   
+}
+ 
+ 
+ 
+abstract class Entrada {
     public $id;
-    public $titulo;
-    public $autor;
-    public $anioPublicacion;
-    public $estado;
-    public $fechaAdquisicion;
+    public $fecha_creacion;
     public $tipo;
-
-    public function __construct($datos) {
+    public $titulo;
+    public $descripcion;
+ 
+    public function __construct($datos = []) {
         foreach ($datos as $key => $value) {
             if (property_exists($this, $key)) {
                 $this->$key = $value;
@@ -16,23 +21,128 @@ class RecursoBiblioteca {
         }
     }
 }
-
-// Implementar las clases Libro, Revista y DVD aquí
-
-class GestorBiblioteca {
-    private $recursos = [];
-
-    public function cargarRecursos() {
-        $json = file_get_contents('biblioteca.json');
-        $data = json_decode($json, true);
-        
-        foreach ($data as $recursoData) {
-            $recurso = new RecursoBiblioteca($recursoData);
-            $this->recursos[] = $recurso;
-        }
-        
-        return $this->recursos;
+//creacion de la clase EntradaUnaColumna
+class EntradaUnaColumna extends Entrada{
+    public $titulo;
+    public $descripcion;
+    obtenerDetallesEspecificos(){
+        return $titulo;
     }
-
-    // Implementar los demás métodos aquí
 }
+ //creacion de la clase EntradaDosColumnas
+class EntradaDosColumnas extends Entrada{
+    public $titulo1;
+    public $descripcion1 ;
+    public $titulo2;
+    public $descripcion2 ;
+    obtenerDetallesEspecificos(){
+        return $titulo1, $titulo2;
+    }
+}
+ //creacion de la clase EntradaTresColumnas
+class EntradaTresColumnas extends Entrada{
+    public $titulo1;
+    public $descripcion1;
+    public $titulo2;
+    public $descripcion2;
+    public $titulo3;
+    public $descripcion3;
+    obtenerDetallesEspecificos(){
+        return $titulo1 ,  $titulo2, $titulo3;
+    }
+   
+}
+ 
+ 
+ 
+ 
+class GestorBlog {
+    private $entradas = [];
+ 
+    public function cargarEntradas() {
+        if (file_exists('blog.json')) {
+            $json = file_get_contents('blog.json');
+            $data = json_decode($json, true);
+            foreach ($data as $entradaData) {
+                $this->entradas[] = new Entrada($entradaData);
+            }
+        }
+    }
+   
+    foreach($entradas as $entrada ){
+        switch ($tipo){
+            case "1":
+ 
+                break;
+            case "2":
+ 
+                break;
+            case "3":
+               
+                break;
+            default:
+ 
+        }
+ 
+    }
+ 
+    public function guardarEntradas() {
+        $data = array_map(function($entrada) {
+            return get_object_vars($entrada);
+        }, $this->entradas);
+        file_put_contents('blog.json', json_encode($data, JSON_PRETTY_PRINT));
+    }
+ 
+    public function obtenerEntradas() {
+        return $this->entradas;
+    }
+ 
+//Recibe entrada Nueva de informacion
+AgregarEntrada($entrada){
+    foreach($entradas  as $entrada){
+        $newId = end($this -> $id) + 1;
+        return $newId;
+    }
+}
+//Edita la informacion de la entrada solicitada
+editarEntrada($entrada){
+    foreach($entradas as $entrada){
+        $this->entrada[] = new Datos();
+    }
+}
+//Elimina entrada de dato por Id
+eliminarEntrada($id){
+    $eliminar = array_filter($id,function[$entrada])
+    return !$id;
+}
+//Filtra la informacion por el Id
+obtenerEntrada($id){
+    $filter = array_filter($id, function[$entrada]);
+    return !$id;
+ 
+}
+//Mueve la informacion de cada elemento dependiendo de la direccion señalada
+moverEntrada($id, $direccion){
+    foreach($entradas as $entrada){
+        if(=="move_up"){
+            $info1 = $this-> id;
+            $direccion= array_filter($id) ;
+            $info2 = $direccion;
+            return $info1,$info2;
+        }
+        else if(=="move_down"){
+            $info2 = $this-> id;
+            $direccion= array_filter($id) ;
+            $info1 = $direccion;
+            return $info1,$info2;
+        }  
+    }
+}
+//Guarda informacion dentro del Json.
+guardarInfo($entrada){
+    $guarda = json_encode($entrada);
+}
+ 
+ 
+}  
+ 
